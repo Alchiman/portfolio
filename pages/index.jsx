2c9a7fe1460e2481b9440f497b53baa129790c5b
Alireza Chaychi <alireza.chaychi@gmail.com>
2022-04-28 18:49:39 -0700

    remove folder

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
 import "../styles/Home.module.css";
 
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Button } from "react-bootstrap";
 
 export default function Home() {
   return (
     <div className="welcome">
       <Container>
-        <h2>Hi, I'm Alireza</h2>
-        <h1>Full Stack Web Developer</h1>
+        <h2>Hi, my name is Alireza</h2>
+        <h1>I&rsquo;m a Full Stack Developer</h1>
         <Button>Learn more</Button>
       </Container>
     </div>
